b8385fddfcd391ccab4192f7d2656c738305f9de
Ammar <36336568+keypad00@users.noreply.github.com>
2019-07-12 00:37:12 +0700

    isi db
    qiw qiww

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "paloserver.database.windows.net";
+    $user = "keypad00";
+    $pass = "Berbagils20";
+    $db = "palodb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
